d8436d2cdb1bb20da3a1f7aba93d9a3ca846327a
Hemanth Bharatha Chakravarthy <hemanthbharathachakravarthy@college.harvard.edu>
2019-04-19 20:53:05 -0400

    Initial working ShinyApp

--- lok_sabha_campaigns/app.R
+++ lok_sabha_campaigns/app.R
@@ -8,6 +8,20 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(readr)
+library(ggthemes)
+library(ggplot2)
+# temp <- read_csv("twitter_data.csv") %>% 
+#   select(created_at, text, favourites_count, retweet_count) %>% 
+#   # Convert text to lower text
+#   mutate(text = tolower(text))
+# temp2 <- temp %>% 
+#   arrange(desc(favourites_count)) %>% 
+#   top_n(50)
+# write_csv(temp2, "data.csv")
+
+data <- read_csv("data.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -18,16 +32,18 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         sliderInput("time",
+                     "Time Posted:", unique(data$created_at))
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+        tabsetPanel(
+          tabPanel("Retweets", 
+                   plotOutput("rtPlot")),
+          tabPanel("Favourites",
+                   plotOutput("favtPlot"))
+        )
       )
    )
 )
@@ -35,13 +51,44 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$rtPlot <- renderPlot({
+     region_subset <- data %>% filter(!is.na(created_at), created_at == input$created_at)
+     ggplot(region_subset, aes(x = created_at, y = retweet_count)) +
+       geom_col() +
+       geom_point() +
+       theme_wsj() + 
+       scale_color_wsj() +
+       labs(
+         title = "When the BJP IT Cells Strike Twitter* —
+         Retweet Count for Selected Time",
+         subtitle = "The Indian elections see bot tweeting as a tool to 
+         make messages popular: when in the day were these bots deadliest?",
+         source = "Data scraped from Twitter; 
+         *represents a particular sample, 
+         details @https://github.com/b-hemanth/lok_sabha_campaigns",
+         x = "Time and Day",
+         y = "Retweets"
+       )
+   })
+   
+   output$favtPlot <- renderPlot({
+     region_subset <- data %>% filter(!is.na(created_at), created_at == input$created_at)
+     ggplot(region_subset, aes(x = created_at, y = favourites_count)) +
+       geom_col() +
+       geom_point() +
+       theme_wsj() + 
+       scale_color_wsj() +
+       labs(
+         title = "When the BJP IT Cells Strike Twitter* —
+         Favourites Count for Selected Time",
+         subtitle = "The Indian elections see bot tweeting as a tool to 
+         make messages popular: when in the day were these bots deadliest?",
+         source = "Data scraped from Twitter; 
+         *represents a particular sample, 
+         details @https://github.com/b-hemanth/lok_sabha_campaigns",
+         x = "Time and Day",
+         y = "Favourites"
+       )
    })
 }
 
